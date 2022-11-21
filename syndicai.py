84a5876caef8028a72dc3db655d715046776894c
Jacob Ávila Camacho <61804385+JacobAvila@users.noreply.github.com>
2022-11-21 16:20:46 -0600

    Add files via upload

--- syndicai.py
+++ syndicai.py
@@ -1,9 +1,7 @@
 import numpy as np
 
-import tensorflow
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from keras.preprocessing import image
-from tensorflow.keras import models
+from keras import models
 
 
 from PIL import Image

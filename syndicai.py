0e880f2a2c06ee9c8fbf8dd341e364be20c8c039
Jacob Ávila Camacho <61804385+JacobAvila@users.noreply.github.com>
2022-11-21 18:31:14 -0600

    Add files via upload

--- syndicai.py
+++ syndicai.py
@@ -25,18 +25,15 @@ class PythonPredictor:
     def predict(self, payload):
 
         # Obtenemos la imagen del post
-        try: 
-            img = Image.open(payload["image"].file)
-        except:
-            img = Image.open(payload["image"].file)
-
-        orig = img.copy()
-
-        img_tensor = image.img_to_array(img)
-        img_tensor = img_tensor.resize((64,64))
+        img = Image.open(payload["image"].file)
+        print("[SIZE]", img.size)
+        img = img.resize((64,64))
+        print("[RESIZE]", img.size)
+        img_tensor = np.array(img)
+        #img_tensor = image.img_to_array(img)
         img_tensor = np.expand_dims(img_tensor, axis=0)
         print("[SHAPE]", img_tensor.shape)
-        #img_tensor = img_tensor/255.
+        img_tensor = img_tensor/255.
   
 
         return 0 #self.model.predict(img_tensor)[0][0]
